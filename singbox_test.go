67badb4a75a3cae86048598e2d183b020e8fea4c
gh-liu <liugh.cs@gmail.com>
2026-07-08 20:49:50 +0800

    Render Reality settings for sing-box VLESS

--- singbox_test.go
+++ singbox_test.go
@@ -32,3 +32,26 @@ func TestRenderBuiltinTemplateSingbox(t *testing.T) {
 		t.Fatalf("unexpected outbound: %+v", ob)
 	}
 }
+
+func TestRenderBuiltinTemplateSingboxVLESSReality(t *testing.T) {
+	nodes, err := subclient.ParseShadowrocket("vless://YXV0bzo2ZjBmNjdkYi1kZjdlLTQ3ZDktYjQ0Ny1hMjMyZTQ0MzNiNTNAaGszLm1peWF6b25vLWthb3JpLmNvbTo0NDM=?tfo=1&remark=%F0%9F%87%AD%F0%9F%87%B0Hong%20Kong%2003&tls=1&xtls=2&sni=hk4e-launcher-static.hoyoverse.com&pbk=zV4Sja0iajpdzkR1iBMbI7RRWL2nlpgwGOtirQSG-zc&sid=b81edf7fdee4&fp=chrome")
+	if err != nil {
+		t.Fatalf("parseShadowrocket returned error: %v", err)
+	}
+
+	var out strings.Builder
+	if err := builtintemplate.RenderBuiltin(&out, "singbox", nodes); err != nil {
+		t.Fatalf("template RenderBuiltin returned error: %v", err)
+	}
+	var decoded map[string]any
+	if err := json.Unmarshal([]byte(out.String()), &decoded); err != nil {
+		t.Fatalf("rendered invalid json: %v\n%s", err, out.String())
+	}
+	outbounds := decoded["outbounds"].([]any)
+	ob := outbounds[0].(map[string]any)
+	tls := ob["tls"].(map[string]any)
+	reality := tls["reality"].(map[string]any)
+	if reality["enabled"] != true || reality["public_key"] != "zV4Sja0iajpdzkR1iBMbI7RRWL2nlpgwGOtirQSG-zc" || reality["short_id"] != "b81edf7fdee4" {
+		t.Fatalf("unexpected reality settings: %+v", reality)
+	}
+}
